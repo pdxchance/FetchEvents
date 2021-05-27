0d2bb9ae585ae00ead843c0f0c5dc891b719c8c2
Deanne Chance <dchance@hireology.com>
2021-05-27 07:10:33 -0500

    Create main interface

--- FetchEvents/EventsViewController.swift
+++ FetchEvents/EventsViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class EventsViewController: UIViewController {
+    
+    let searchBar : UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        return searchBar
+    }()
+    
+    let tableView : UITableView = {
+       let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .blue
+        view.addSubview(searchBar)
+        view.addSubview(tableView)
+        
+        
+        searchBar.anchor(top: view.safeAreaLayoutGuide.topAnchor, bottom: nil, leading: view.leadingAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0))
+        
+        tableView.anchor(top: searchBar.bottomAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, leading: view.leadingAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0))
     }
 
 }
