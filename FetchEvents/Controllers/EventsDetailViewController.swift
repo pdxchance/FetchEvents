87d0a4a8f6788ef63a61d3e985a42d8765463107
chance <pdxchance@icloud.com>
2022-02-19 17:06:02 -0600

    Create separate Detail view

--- FetchEvents/Controllers/EventsDetailViewController.swift
+++ FetchEvents/Controllers/EventsDetailViewController.swift
@@ -15,46 +15,6 @@ class EventsDetailViewController: UIViewController {
     
     var isSelected: Bool?
 
-    let contentStackView : UIStackView = {
-       let contentStackView = UIStackView()
-        contentStackView.axis = .vertical
-        contentStackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        return contentStackView
-    }()
-    
-    let eventTitle : UILabel = {
-        let eventTitle = UILabel()
-        eventTitle.font = .systemFont(ofSize: 24.0, weight: .black)
-        eventTitle.numberOfLines = 0
-        eventTitle.lineBreakMode = .byWordWrapping
-        eventTitle.translatesAutoresizingMaskIntoConstraints = false
-        
-        return eventTitle
-    }()
-    
-    let eventImage : UIImageView = {
-       let eventImage = UIImageView()
-        eventImage.contentMode = .scaleToFill
-        eventImage.translatesAutoresizingMaskIntoConstraints = false
-        
-        return eventImage
-    }()
-    
-    let eventLocation : UILabel = {
-        let eventLocation = UILabel()
-        eventLocation.translatesAutoresizingMaskIntoConstraints = false
-        
-        return eventLocation
-    }()
-    
-    let eventDate : UILabel = {
-        let eventDate = UILabel()
-        eventDate.font = .systemFont(ofSize: 17.0, weight: .thin)
-        eventDate.translatesAutoresizingMaskIntoConstraints = false
-        
-        return eventDate
-    }()
     
     let favoriteImage : UIButton = {
        let favoriteImage = UIButton()
@@ -77,26 +37,20 @@ class EventsDetailViewController: UIViewController {
         
         favoriteImage.addTarget(self, action: #selector(toggleFavorites), for: .touchUpInside)
         favoriteImage.isSelected = isSelected ?? false
-
-        view.addSubview(contentStackView)
-        contentStackView.addArrangedSubview(eventTitle)
-        contentStackView.addArrangedSubview(eventImage)
-        contentStackView.addArrangedSubview(eventLocation)
-        contentStackView.addArrangedSubview(eventDate)
-        view.addSubview(favoriteImage)
+        
+        let eventDetailView = EventDetailView(frame: CGRect.zero)
+        view.addSubview(eventDetailView)
+        
+        eventDetailView.anchor(top: view.safeAreaLayoutGuide.topAnchor, bottom: nil, leading: view.leadingAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0))
         
         let viewModel = EventViewModel(event: event!)
         
-        eventTitle.text = viewModel.eventTitle
-        eventLocation.text = viewModel.eventLocation
-        eventDate.text = viewModel.eventDateTime
+        eventDetailView.eventTitle.text = viewModel.eventTitle
+        eventDetailView.eventLocation.text = viewModel.eventLocation
+        eventDetailView.eventDate.text = viewModel.eventDateTime
         
         let url = URL(string: (event?.performers![0].image!)!)
-            self.eventImage.kf.setImage(with: url, placeholder: UIImage(named: "placeholder"), completionHandler: nil)
-        
-        contentStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, bottom: nil, leading: view.leadingAnchor, trailing: view.trailingAnchor)
-        
-        favoriteImage.anchor(top: eventImage.topAnchor, bottom: nil, leading: nil, trailing: eventImage.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0), size: .init(width: 50, height: 50))
+        eventDetailView.eventImage.kf.setImage(with: url, placeholder: UIImage(named: "placeholder"), completionHandler: nil)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
---
+++ FetchEvents/EventDetailView.swift
@@ -0,0 +1,82 @@
+//
+//  EventDetailView.swift
+//  FetchEvents
+//
+//  Created by Deanne Chance on 2/19/22.
+//
+
+import UIKit
+
+class EventDetailView: UIView {
+    
+    let contentStackView : UIStackView = {
+       let contentStackView = UIStackView()
+        contentStackView.axis = .vertical
+        contentStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return contentStackView
+    }()
+    
+    let eventTitle : UILabel = {
+        let eventTitle = UILabel()
+        eventTitle.font = .systemFont(ofSize: 24.0, weight: .black)
+        eventTitle.numberOfLines = 0
+        eventTitle.lineBreakMode = .byWordWrapping
+        eventTitle.translatesAutoresizingMaskIntoConstraints = false
+        
+        return eventTitle
+    }()
+    
+    let eventImage : UIImageView = {
+       let eventImage = UIImageView()
+        eventImage.contentMode = .scaleToFill
+        eventImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        return eventImage
+    }()
+    
+    let eventLocation : UILabel = {
+        let eventLocation = UILabel()
+        eventLocation.translatesAutoresizingMaskIntoConstraints = false
+        
+        return eventLocation
+    }()
+    
+    let eventDate : UILabel = {
+        let eventDate = UILabel()
+        eventDate.font = .systemFont(ofSize: 17.0, weight: .thin)
+        eventDate.translatesAutoresizingMaskIntoConstraints = false
+        
+        return eventDate
+    }()
+    
+    let favoriteImage : UIButton = {
+       let favoriteImage = UIButton()
+        favoriteImage.contentMode = .scaleAspectFit
+        favoriteImage.setImage(UIImage(named: "icons8-like"), for: .selected)
+        favoriteImage.setImage(UIImage(named: "icons8-hearts"), for: .normal)
+        favoriteImage.translatesAutoresizingMaskIntoConstraints = false
+
+        return favoriteImage
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(contentStackView)
+        contentStackView.addArrangedSubview(eventTitle)
+        contentStackView.addArrangedSubview(eventImage)
+        contentStackView.addArrangedSubview(eventLocation)
+        contentStackView.addArrangedSubview(eventDate)
+        addSubview(favoriteImage)
+        
+        contentStackView.anchor(top: self.topAnchor, bottom: nil, leading: self.leadingAnchor, trailing: self.trailingAnchor)
+        
+        favoriteImage.anchor(top: eventImage.topAnchor, bottom: nil, leading: nil, trailing: eventImage.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0), size: .init(width: 50, height: 50))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
